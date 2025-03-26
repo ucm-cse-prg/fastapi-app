d213b21cf038878c100382516c64570b85ecb410
Avash Adhikari <avashraj328@gmail.com>
2025-03-26 01:27:26 -0700

    feat: added Product and Category pydantic data models

--- app/models.py
+++ app/models.py
@@ -15,15 +15,61 @@ to validate and serialize the data passed between the API client and the server.
 # - Price should be a positive number less than 100000, and ending in ".99", i.e., 9.99, 19.99, etc.
 #
 # Ensure you use proper data validation and type annotations.
-#
-# Example:
-#
-# from pydantic import BaseModel
-#
-# class Category(BaseModel):
-#     # TODO: Define fields and validations for a product category
-#     pass
-#
-# class Product(BaseModel):
-#     # TODO: Define fields and validations for a product
-#     pass
+
+
+
+from pydantic import BaseModel, Field, field_validator, ValidationError
+
+
+class Category(BaseModel):
+    '''
+    Category is a data model for a Product category
+
+    Attributes:
+    name (str) - name of the category
+    description (str) - description of the category
+    '''
+    name: str     
+
+    description: str 
+
+class Product(BaseModel):
+    '''
+    Product is a data model for a Product
+
+    Attributes:
+    name (str) - name of the prodcut
+    description (str) - a description of the product
+    price (float) - the price of the product
+    category (Category) - the category of the product
+    '''
+    name: str
+    
+    description: str
+
+    price: float = Field (
+        lt=100000,
+        gt=0,
+        allow_inf_nan=False,
+    )
+
+    category: Category
+
+    @field_validator('price', mode='after')
+    @classmethod
+    def endsInNinetyNine(cls, value: float) -> float:
+        '''
+        endsInNinetyNine makes sure that the price ends in .9.99
+
+        arguments:
+        value (float) - the value we are validating
+
+        returns:
+        value (float) - the value which ends in .99
+        '''
+        if round(value % 1.00, 2) != .99:
+            raise ValueError(f"{value} does not end in .99")
+        return value
+
+
+
