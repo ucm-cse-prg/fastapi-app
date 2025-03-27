b9542f45afc7beafa49833f9c0fe23ff630b53e5
Avash Adhikari <avashraj328@gmail.com>
2025-03-26 19:36:27 -0700

    feat: business logic to create, get, delete, update product added

--- app/actions.py
+++ app/actions.py
@@ -1,10 +1,11 @@
 import typing
 from functools import wraps
-
 from app.exceptions import InternalServerError
+from Beanie import PydanticObjectId
+from app.documents import Product
+from app.models import Category
 
-
-# Wrapper function to run action and rais InternalServerError if it fails
+# Wrapper function to run action and raise InternalServerError if it fails
 @typing.no_type_check
 def run_action(action):
     @wraps(action)
@@ -36,8 +37,45 @@ def run_action(action):
 #
 # Define functions like create_product, get_product, update_product, delete_product with appropriate signatures.
 #
-# Example:
-#
-# async def create_product(name: str, price: float, category: YourCategoryType, description: str = "") -> YourProductType:
-#     # TODO: Implement creation logic
-#     pass
+
+
+
+async def create_product(name: str, price: float, category: CategoryType, description: str = "") -> Product:
+    newProdcut: Product = await Product(name=name,description=description, price=price, category=category).insert()
+
+    if not newProduct:
+        raise InternalServerError("Failed to create product")
+
+    return newProduct
+
+
+async def get_product(product: Product) -> Product:
+
+    return product
+
+
+async def update_product(product: Product,
+                         name: Optional[str] = None, 
+                         description: Optional[str] = None,
+                         price: Optional[float] = None,
+                         category: Optional[Category] = None) -> Product:
+    if name: 
+        product.name = name
+    if description:
+        product.description = description
+    if price:
+        product.price = price
+    if category:
+        product.category = category
+    
+    await product.save()
+
+    if not product:
+        raise InternalServerError("Failed to update product")
+    return product
+
+async def delete_product(product: Product) -> Product:
+    await delete(product)
+
+    if await Product.get(product.id):
+        raise InternalServerError("Failed to delete product")
